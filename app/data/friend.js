6f1cf7d3d8ebbfdbadea8dde6e3313d75f982f27
katherine Lundy <katherinersl@gmail.com>
2020-07-18 13:48:58 -0400

    completed testing of local and working on readMe

--- app/data/friend.js
+++ app/data/friend.js
@@ -5,104 +5,104 @@
 
 
 var friends = [
-    {
-      name: "Ahmed",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAq7AAAAJDAwYzI4NTQ4LWYwZWUtNGFkYS1hNTYwLTZjYzkwY2ViZDA3OA.jpg",
-      scores: [
-        "5",
-        "1",
-        "4",
-        "4",
-        "5",
-        "1",
-        "2",
-        "5",
-        "4",
-        "1"
-      ]
-    },
-    {
-      name: "Jacob Deming",
-      photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
-      scores: [
-        "4",
-        "2",
-        "5",
-        "1",
-        "3",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2"
-      ]
-    },
-    {
-      name: "Jeremiah Scanlon",
-      photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
-      scores: [
-        "5",
-        "2",
-        "2",
-        "2",
-        "4",
-        "1",
-        "3",
-        "2",
-        "5",
-        "5"
-      ]
-    },
-    {
-      name: "Louis T. Delia",
-      photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-      scores: [
-        "3",
-        "3",
-        "4",
-        "2",
-        "2",
-        "1",
-        "3",
-        "2",
-        "2",
-        "3"
-      ]
-    },
-    {
-      name: "Lou Ritter",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
-      scores: [
-        "4",
-        "3",
-        "4",
-        "1",
-        "5",
-        "2",
-        "5",
-        "3",
-        "1",
-        "4"
-      ]
-    },
-    {
-      name: "Jordan Biason",
-      photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
-      scores: [
-        "4",
-        "4",
-        "2",
-        "3",
-        "2",
-        "2",
-        "3",
-        "2",
-        "4",
-        "5"
-      ]
-    }
-  
-  ];
-  
-  // Here we export the array. This makes it accessible to other files using require.
-  module.exports = friends;
+  {
+    name: "Ahmed",
+    photo: "https://www.lovethispic.com/uploaded_images/218149-Hot-Guy-To-Wake-Up-To.jpg",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+    ]
+  },
+  {
+    name: "Jacob Deming",
+    photo: "https://media.thetab.com/blogs.dir/279/files/2018/06/person-people-human-39.jpg",
+    scores: [
+      "4",
+      "2",
+      "5",
+      "1",
+      "3",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2"
+    ]
+  },
+  {
+    name: "Jeremiah Scanlon",
+    photo: "https://data.whicdn.com/images/329906242/original.jpg",
+    scores: [
+      "5",
+      "2",
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "2",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Louis T. Delia",
+    photo: "https://qph.fs.quoracdn.net/main-qimg-18206a2bd9c28944eb00d93d70ba258b.webp",
+    scores: [
+      "3",
+      "3",
+      "4",
+      "2",
+      "2",
+      "1",
+      "3",
+      "2",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Lou Ritter",
+    photo: "https://data.whicdn.com/images/307428490/original.jpg",
+    scores: [
+      "4",
+      "3",
+      "4",
+      "1",
+      "5",
+      "2",
+      "5",
+      "3",
+      "1",
+      "4"
+    ]
+  },
+  {
+    name: "Jordan Biason",
+    photo: "https://i.pinimg.com/originals/4f/ee/7e/4fee7e5e99e4f15416bf4b46d7346ad6.jpg",
+    scores: [
+      "4",
+      "4",
+      "2",
+      "3",
+      "2",
+      "2",
+      "3",
+      "2",
+      "4",
+      "5"
+    ]
+  }
+
+];
+
+// Here we export the array. This makes it accessible to other files using require.
+module.exports = friends;
